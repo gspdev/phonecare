ddbba73b846da7fa97a0e4e72bae135384b09a3b
mivtec <imivtec@gmail.com>
2017-11-10 11:30:55 +0800

    delete repair

--- app/code/local/Iphonecare/Repair/Model/Resource/Setup.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Setup Resurce Model
- *
- */
-class Iphonecare_Repair_Model_Resource_Setup extends Mage_Sales_Model_Resource_Setup
-{
-}
--- app/code/local/Iphonecare/Repair/Model/Stores.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Iphonecare_Repair_Model_Stores extends Mage_Core_Model_Abstract
-{
-    protected function _construct()
-    {
-        $this->_init('iphonecarerepair/stores');
-    }
-}
