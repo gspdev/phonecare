6feca41fc813af4a72961f2844c01243b78f4d4a
mivtec <imivtec@gmail.com>
2017-11-22 13:56:18 +0800

    add tool for product price by mivec

---
+++ mivec/catalog/product/config.php
@@ -0,0 +1,9 @@
+<?php
+require dirname(dirname(__FILE__)) . "/config.php";
+
+//table
+define("__TABLE_PRODUCT__" , "catalog_product_entity");
+define("__TABLE_PRODUCT_PRICE__" , "catalog_product_entity_decimal");
+
+//attribute
+define("__ATTR_PRICE__" , 75);
