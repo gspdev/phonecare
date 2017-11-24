175e35f6393377eec8011c7336f0a70d94c01fb3
mivtec <imivtec@gmail.com>
2017-11-24 17:00:04 +0800

    tool kit. by mivec

--- mivec/catalog/product/media/update.img.from.category.php
+++ mivec/catalog/product/media/update.img.from.category.php
@@ -1,16 +1,48 @@
 <?php
 require dirname(__FILE__) . '/config.php';
+//echo __PATH_MEDIA_PRODUCT_REPAIR__;exit;
 
 //从category的图复制记录更新到产品数据表中
 $sql = "";
 
-$_categoryId = 2511;
+/*categories*/
+$sql = "SELECT * FROM " . __TABLE_CATEGORY__
+    ." WHERE `level` = 4"
+    ." AND parent_id=2622"
+    ." ORDER BY entity_id DESC";
+//echo $sql;exit;
 
-$_category = getCategory($_categoryId);
-//print_r($_category->getData());
+if ($row = $db->fetchAll($sql)) {
+    foreach ($row as $rs) {
+        $_categoryId = $rs['entity_id'];
+        //$_categoryId = 2511;
+        $_category = getCategory($_categoryId);
+        $_categoryImg = $_category->getData("image");
 
-$_productIds = getProductIdByCategory($_category);
-print_r($_productIds);exit;
+        //创建目标目录，复制图片,更新数据库;
+        $_dir['source'] = __PATH_MEDIA_CATEGORY__;
+        $_dir['dest'] = __PATH_MEDIA_PRODUCT_REPAIR__;
+        createDir($_dir['dest']);
+
+        $_image['source'] = $_dir['source'] . $_categoryImg;
+        $_image['dest'] = $_dir['dest'] . $_categoryImg;
+        cloneImage($_image['source'] , $_image['dest']);
+
+        if ($_productIds = getProductIdByCategory($_category)) {
+            foreach ($_productIds as $_id) {
+                //update database;
+                $_image['filename'] = $_repairPrefix . $_categoryImg;
+                //echo $_image['filename'];exit;
+                if (updateProductImage($_id , $_image['filename'])) {
+                    echo $_id . " update image successfully<br>";
+                    //exit;
+                }
+            }
+        }
+        usleep(5);
+    }
+}
+
 
 
 function getCategory($_categoryId)
@@ -18,7 +50,6 @@ function getCategory($_categoryId)
     global $db;
     return Mage::getModel('catalog/category')->load($_categoryId);
 }
-
 
 function getProductIdByCategory($_category)
 {
@@ -34,4 +65,68 @@ function getProductIdByCategory($_category)
         return $data;
     }
     return false;
+}
+
+function updateProductImage($_entityId , $_image)
+{
+    global $db;
+    $sql = "REPLACE INTO " . __TABLE_PRODUCT_MEDIA__
+        ." SET `value`='$_image'"
+        .",entity_id=" . $_entityId
+        .",attribute_id=" . __ATTR_PRODUCT_IMG__;
+
+        //." WHERE entity_id=" . $_entityId
+        //." AND attribute_id=" . __ATTR_PRODUCT_IMG__;
+
+    //echo $sql;exit;
+    //set images for product
+    setProductImageValue($_entityId , $_image);
+
+    try {
+        if ($db->query($sql)) {
+            return true;
+        }
+    } catch (Exception $e) {
+        print_r($e);
+    }
+}
+
+function setProductImageValue($_entityId , $_image)
+{
+    global $db,$_imgType;
+
+    $_return = false;
+
+    foreach ($_imgType as $_code => $_val) {
+        $sql = "REPLACE INTO " . __TABLE_PRODUCT_VARCHAR__
+            ." SET `value`='$_image'"
+            .",entity_id=" . $_entityId
+            .",attribute_id=" . $_val
+            .",store_id=0,entity_type_id=4";
+       // echo $sql;
+
+        if ($db->query($sql)) {
+            $_return = TRUE;
+        }
+    }
+    return $_return;
+}
+
+function getFinalDir($_path)
+{
+    return basename($_path) . PHP_EOL;
+}
+
+function createDir($_destinationDir)
+{
+    if (!file_exists($_destinationDir)) {
+        mkdir($_destinationDir);
+    }
+}
+
+function cloneImage($_source , $_destination)
+{
+    if (!file_exists($_destination)) {
+        return copy($_source , $_destination);
+    }
 }
